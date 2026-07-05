a7a673ff48b033685896925a24d1b95c8681bf37
gn1382 <gnasereslami@gmail.com>
2026-07-06 00:17:18 +0330

    first commit

---
+++ advanced_sql/month.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM job_postings_fact
+LIMIT 2;
+
+CREATE TABLE january_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH from job_posted_date) = 1;
+
+CREATE TABLE february_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH from job_posted_date) = 2;
+
+CREATE TABLE march_jobs AS
+    SELECT *
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH from job_posted_date) = 3;
+
+
